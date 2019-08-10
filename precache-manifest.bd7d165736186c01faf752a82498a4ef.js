a289c41a6de2c12d6a2546376ffb206b2555da8d
Isabella Merendino <isabellamerendino@Isabellas-MacBook-Pro.local>
2019-08-10 13:31:49 -0400

    Updates

---
+++ precache-manifest.bd7d165736186c01faf752a82498a4ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e324a1ac69bc5db87999df1f71ee91",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "f4520cbf0fe14f07c985",
+    "url": "/ClickyGame/static/css/main.7fcfa169.chunk.css"
+  },
+  {
+    "revision": "1a646aad386651356451",
+    "url": "/ClickyGame/static/js/2.52c33ced.chunk.js"
+  },
+  {
+    "revision": "f4520cbf0fe14f07c985",
+    "url": "/ClickyGame/static/js/main.f3bfac7c.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
